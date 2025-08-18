1ed055d577c4d4576210623a181632926221d276
Ritu <ks8228939@gmail.com>
2025-08-18 22:03:50 +0530

    variable in java

---
+++ variable.java
@@ -0,0 +1,13 @@
+public class variable {
+    public static void main(String[] args)
+    {
+        var a=10;
+        var b=20;
+        var sum=a+b;
+        var diff=a-b;
+        int ans=(a+b)/(a+b);
+        System.out.println("Sum: "+sum);
+        System.out.println("Diff: "+diff);
+        System.out.println("answer: "+ans);
+    }
+}
