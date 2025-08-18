e3e8be8205ea0e178ac36f2363bbe143cf6ecdb9
Ritu <ks8228939@gmail.com>
2025-08-18 21:17:27 +0530

    printing in java

---
+++ print.java
@@ -0,0 +1,8 @@
+class print{
+    public static void main(String[] args)
+    {
+        System.out.print("Hello World with java");
+        System.out.println("Hello World with java");
+        System.out.print("Hello World with java");
+    }
+}
